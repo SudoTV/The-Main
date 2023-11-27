93c3f492d910be0ac2d82800a3245573712cfc8d
wmxpy <wm@mengw.io>
2023-11-26 21:28:05 -0600

    Add i18n support and rewrite rules for accepted locales

--- next.config.js
+++ next.config.js
@@ -1,19 +1,39 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 const { LOCALE } = require("@sudoo/locale");
 
 const ENTRY_LOCALE = "$default";
 
+const ACCEPTED_LOCALES = [
+    LOCALE.ENGLISH_UNITED_STATES,
+    LOCALE.CHINESE_SIMPLIFIED,
+];
+
 const nextConfig = {
     i18n: {
         locales: [
             ENTRY_LOCALE,
-            LOCALE.ENGLISH_UNITED_STATES,
-            LOCALE.CHINESE_SIMPLIFIED,
+            ...ACCEPTED_LOCALES,
         ],
         defaultLocale: ENTRY_LOCALE,
         localeDetection: false,
     },
     trailingSlash: true,
+    async rewrites() {
+
+        const rewriteList = [];
+        for (const acceptedLocale of ACCEPTED_LOCALES) {
+            rewriteList.push({
+                source: `/${acceptedLocale}`,
+                destination: "/",
+            }, {
+                source: `/${acceptedLocale}/:path*`,
+                destination: "/:path*",
+            });
+        }
+        return rewriteList;
+    },
 };
 
 module.exports = nextConfig;
