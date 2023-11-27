a957eaf849855660be3f372aed6915b37d42fd7d
wmxpy <wm@mengw.io>
2023-11-27 00:50:42 -0600

    Add new page and not-found component

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,27 @@
+/**
+ * @author WMXPY
+ * @description Page
+ */
+
+import { headers } from "next/headers";
+
+export default function Home() {
+
+    const buildDate = Date.now();
+    const formattedDate = new Intl.DateTimeFormat("en-US", {
+        dateStyle: "long",
+        timeStyle: "long",
+    }).format(buildDate);
+
+    console.log(headers());
+
+    return (<main
+        className="flex min-h-screen flex-col items-center justify-between p-24"
+    >
+        <h1>Static page</h1>
+        <p>This page is static. It was built on {formattedDate}.</p>
+        <p>
+            <a href="/ssr">View a server-side rendered page.</a>
+        </p>
+    </main>);
+};
