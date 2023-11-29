192b516d0813d0dd771618eb5fc6e194c141e485
wmxpy <wm@mengw.io>
2023-11-29 00:09:28 -0600

    Add 404 and 500 error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+/**
+ * @author WMXPY
+ * @description 404
+ */
+
+export default function Custom404() {
+
+    return (<main
+        className="flex min-h-screen flex-col items-center justify-between p-24"
+    >
+        <h1>404 - Not Found</h1>
+    </main>);
+};
---
+++ src/pages/500.tsx
@@ -0,0 +1,13 @@
+/**
+ * @author WMXPY
+ * @description 500
+ */
+
+export default function Custom500() {
+
+    return (<main
+        className="flex min-h-screen flex-col items-center justify-between p-24"
+    >
+        <h1>500 - Server Error</h1>
+    </main>);
+};
