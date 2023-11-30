f00641acf9c7ef8be58ebc327860d1f7d1e22625
wmxpy <wm@mengw.io>
2023-11-29 23:43:54 -0600

    Refactor page component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,12 @@
  * @description Page
  */
 
-import { IETF_LOCALE } from "@sudoo/locale";
 import { WelcomeCode } from "../components/welcome/welcome-code";
-import { useLocale } from "../i18n/use-locale";
 
-export default async function Home(props: any) {
-
-    const buildDate = Date.now();
-    const formattedDate = new Intl.DateTimeFormat("en-US", {
-        dateStyle: "long",
-        timeStyle: "long",
-    }).format(buildDate);
-
-    const locale: IETF_LOCALE = useLocale();
+export default async function Home() {
 
     return (<main
-        className="flex flex-col items-center justify-between p-24"
+        className="flex flex-col items-center justify-between p-4 sm:p-24"
     >
         <WelcomeCode />
     </main>);
