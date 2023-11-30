dffc644978b76d0c89aa44af511d19d6732bc063
wmxpy <wm@mengw.io>
2023-11-29 19:58:50 -0600

    Change namespace to Reprint and update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,30 +4,17 @@
  * @description Page
  */
 
-import { MDX_H2 } from "../../components/mdx/h2";
+import { MDXLocaleSwitcher } from "../../components/mdx/locale-switcher";
 import EnglishUnitedStates from "../../dictionary/about/about/en-US.mdx";
-import { useLocale } from "../../i18n/use-locale";
+import ChineseSimplified from "../../dictionary/about/about/zh-CN.mdx";
 
 type Props = {
 };
 
 export default async function Page(props: Props) {
 
-  const locale = useLocale();
-
-  const buildDate = Date.now();
-  const formattedDate = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "long",
-    timeStyle: "long",
-  }).format(buildDate);
-
-  return (<main
-    className="p-6 mx-auto w-full max-w-screen-xl text-left rtl:text-right"
-  >
-    <EnglishUnitedStates
-      components={{
-        h2: MDX_H2,
-      }}
-    />
-  </main>);
+  return (<MDXLocaleSwitcher
+    zh-CN={ChineseSimplified}
+    en-US={EnglishUnitedStates}
+  />);
 };
