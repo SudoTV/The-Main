d1fc8743f6c03909ea8e345511f87c3101b8c842
WMXPY <wm@mengw.io>
2023-12-02 22:45:51 -0600

    Fix copyright year in footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -27,7 +27,7 @@ export const LayoutFooter: React.FC = () => {
                 <div className="grid grid-cols-2 gap-8 px-4 py-2 lg:py-4 md:grid-cols-4">
                     <div>
                         <h2 className="mb-6 text-sm font-semibold text-gray-900 dark:text-white">
-                            {">"} SudoTV © 2020-2024
+                            SudoTV © 2020-2024
                         </h2>
                         <ul className="text-gray-500 dark:text-gray-400">
                             <li className="mb-2">
