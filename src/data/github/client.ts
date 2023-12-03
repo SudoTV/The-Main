fb187225f588b7a5f5c2caa3182bcfac6a95f763
WMXPY <wm@mengw.io>
2023-12-03 01:29:29 -0600

    Fix cache import and variable name

--- src/data/github/client.ts
+++ src/data/github/client.ts
@@ -6,6 +6,7 @@
 
 import { graphql } from "@octokit/graphql";
 import NodeCache from "node-cache";
+import { cache } from "react";
 import { GRAPHQL_GITHUB_API_TOKEN } from "../../util/environment";
 import { logger } from "../../util/log";
 import { digestMD5String } from "../../util/md5";
@@ -17,35 +18,37 @@ export const graphqlWithAuth = graphql.defaults({
     },
 });
 
-const cache: NodeCache = new NodeCache();
+const nodeCache: NodeCache = new NodeCache();
 const CACHE_EXPIRE: number = 60 * 5; // 5 minutes
 
-export const githubGraphql = async <T>(query: string): Promise<CacheableResponse<T>> => {
+export const githubGraphql = cache(
+    async <T>(query: string): Promise<CacheableResponse<T>> => {
 
-    const queryHash: string = await digestMD5String(query);
+        const queryHash: string = await digestMD5String(query);
 
-    const cached: T | undefined = cache.get(queryHash);
-    if (typeof cached !== "undefined") {
+        const cached: T | undefined = nodeCache.get(queryHash);
+        if (typeof cached !== "undefined") {
 
-        logger.info(`[Github] Query cache hit: ${queryHash}`);
+            logger.info(`[Github] Query cache hit: ${queryHash}`);
+
+            return {
+                cached: CACHED_TYPE.FULL,
+                cachedComponents: [{
+                    identifier: "query",
+                }],
+                data: cached,
+            };
+        }
+
+        logger.info(`[Github] Query cache miss: ${queryHash}`);
+        const response: T = await graphqlWithAuth(query);
+
+        nodeCache.set(queryHash, response, CACHE_EXPIRE);
 
         return {
-            cached: CACHED_TYPE.FULL,
-            cachedComponents: [{
-                identifier: "query",
-            }],
-            data: cached,
+            cached: CACHED_TYPE.NONE,
+            cachedComponents: [],
+            data: response,
         };
-    }
-
-    logger.info(`[Github] Query cache miss: ${queryHash}`);
-    const response: T = await graphqlWithAuth(query);
-
-    cache.set(queryHash, response, CACHE_EXPIRE);
-
-    return {
-        cached: CACHED_TYPE.NONE,
-        cachedComponents: [],
-        data: response,
-    };
-};
+    },
+);
