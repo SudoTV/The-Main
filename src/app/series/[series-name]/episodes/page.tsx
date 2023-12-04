85a7a8d4d817af391f205f87a980f08c317d4524
wmxpy <wm@mengw.io>
2023-12-04 01:44:32 -0600

    Refactor series page component and add series metadata retrieval

--- src/app/series/[series-name]/episodes/page.tsx
+++ src/app/series/[series-name]/episodes/page.tsx
@@ -5,8 +5,15 @@
  */
 
 import { LocaleSwitcher } from "../../../../components/preference/language-switcher";
+import { MainPageWrapper } from "../../../../components/typography/main-page-wrapper";
+import { CacheableResponse } from "../../../../data/cache/definition";
+import { SERIES_TYPE, SeriesEntity } from "../../../../data/definition/series/series";
 import { getGithubFile } from "../../../../data/github/get-file";
+import { requestSeriesMetadata } from "../../../../data/request/series-metadata";
+import { seriesInternationalization } from "../../../../dictionary/series/_intl";
+import { SERIES_PROFILE } from "../../../../dictionary/series/_profile";
 import { useLocale } from "../../../../i18n/use-locale";
+import { HrefConfig } from "../../../../util/href";
 
 type Props = {
 
@@ -18,23 +25,34 @@ type Props = {
 export default async function Page(props: Props) {
 
     const locale = useLocale();
+    const seriesFormat = seriesInternationalization.format(locale);
 
-    const buildDate = Date.now();
-    const formattedDate = new Intl.DateTimeFormat("en-US", {
-        dateStyle: "long",
-        timeStyle: "long",
-    }).format(buildDate);
+    const series: CacheableResponse<SeriesEntity<SERIES_TYPE>> =
+        await requestSeriesMetadata(props.params["series-name"]);
 
     await getGithubFile("SudoTV", "SudoTV-Series-DB", "main", ["docs", "README.md"]);
 
-    return (<main
-        className="flex min-h-screen flex-col items-center justify-between p-24"
+    return (<MainPageWrapper
+        locale={locale}
+        breadcrumbElements={[
+            {
+                name: seriesFormat.get(SERIES_PROFILE.SERIES_TITLE),
+                href: HrefConfig.withinSite(locale, "series"),
+            },
+            {
+                name: series.data.title[locale] as string,
+                href: HrefConfig.withinSite(locale, "series", series.data.identifier),
+            },
+            {
+                name: seriesFormat.get(SERIES_PROFILE.ALL_EPISODES),
+                href: HrefConfig.withinSite(locale, "series", series.data.identifier, "episodes"),
+            },
+        ]}
     >
         <h1>Static page</h1>
-        <p>This page is static. It was built on {formattedDate}.</p>
         <p>{props.params["series-name"]}</p>
         <p>Episode</p>
         <p>{locale}</p>
         <LocaleSwitcher />
-    </main>);
+    </MainPageWrapper>);
 };
