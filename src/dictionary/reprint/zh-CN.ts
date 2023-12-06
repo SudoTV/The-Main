cfe17fdba05df94de59ab498955e7b7a1b69156a
wmxpy <wm@mengw.io>
2023-12-05 22:37:10 -0600

    Fix typos and update organization name

--- src/dictionary/reprint/zh-CN.ts
+++ src/dictionary/reprint/zh-CN.ts
@@ -10,15 +10,15 @@ import { REPRINT_PROFILE } from "./_profile";
 export const profile_reprint_zh_CN: PROFILE<REPRINT_PROFILE> = {
 
     [REPRINT_PROFILE.REPRINT]: "转载",
-    [REPRINT_PROFILE.REPRINT_INDEX_DESCRIPTION]: "您转载 SudoTV 计划的作品的权利和限制根据转载的作品不同而不同。请参阅以下章节以了解更多信息。",
+    [REPRINT_PROFILE.REPRINT_INDEX_DESCRIPTION]: "您转载 SudoTV 的作品的权利和限制根据转载的作品不同而不同。请参阅以下章节以了解更多信息。",
     [REPRINT_PROFILE.REPRINT_INDEX_EXTRA]: "如果您认为想转载的作品不属于以上的类型之一。或者您有任何关于转载的问题，请访问我们的 联系我们 页面。或者通过发送邮件到 info@sudo.tv 的方式联系我们。",
 
     [REPRINT_PROFILE.SERIES_CODE_AND_CONTENT]: "剧集代码和内容",
-    [REPRINT_PROFILE.SERIES_CODE_AND_CONTENT_DESCRIPTION]: "您转载 SudoTV 计划发布的剧集相关作品的权利和限制",
+    [REPRINT_PROFILE.SERIES_CODE_AND_CONTENT_DESCRIPTION]: "您转载 SudoTV 发布的剧集相关作品的权利和限制",
 
     [REPRINT_PROFILE.SITE_CONTENT_AND_SOURCE_CODE]: "网站内容及其源代码",
-    [REPRINT_PROFILE.SITE_CONTENT_AND_SOURCE_CODE_DESCRIPTION]: "您转载 SudoTV 计划网站及其源代码的权利和限制",
+    [REPRINT_PROFILE.SITE_CONTENT_AND_SOURCE_CODE_DESCRIPTION]: "您转载 SudoTV 网站及其源代码的权利和限制",
 
     [REPRINT_PROFILE.SERIES_VIDEOS]: "视频",
-    [REPRINT_PROFILE.SERIES_VIDEOS_DESCRIPTION]: "您转载 SudoTV 计划发布的视频的权利和限制",
+    [REPRINT_PROFILE.SERIES_VIDEOS_DESCRIPTION]: "您转载 SudoTV 发布的视频的权利和限制",
 };
