3d765f303a80a1a198be23806c09fcc5ddbcdd51
wmxpy <wm@mengw.io>
2023-12-06 00:57:48 -0600

    Update language support message

--- src/dictionary/content/zh-CN.ts
+++ src/dictionary/content/zh-CN.ts
@@ -9,6 +9,6 @@ import { CONTENT_PROFILE } from "./_profile";
 
 export const profile_content_zh_CN: PROFILE<CONTENT_PROFILE> = {
 
-    [CONTENT_PROFILE.CONTENT_NOT_AVAILABLE_FOR_LANGUAGE_2]: "本文档不支持您偏好的语言{}, 当前以{}版本显示",
+    [CONTENT_PROFILE.CONTENT_NOT_AVAILABLE_FOR_LANGUAGE_2]: "本页面不支持您偏好的语言{}, 当前正在显示{}版本",
     [CONTENT_PROFILE.DISPLAYING_WITH_LOCALE_1]: "正在显示{}版本",
 };
