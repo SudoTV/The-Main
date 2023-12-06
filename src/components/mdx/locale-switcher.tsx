4115132b6fe8925cb7083d99e7aface89f706e8b
wmxpy <wm@mengw.io>
2023-12-06 00:51:11 -0600

    Add support for default locale in MDXLocaleSwitcher component

--- src/components/mdx/locale-switcher.tsx
+++ src/components/mdx/locale-switcher.tsx
@@ -6,20 +6,38 @@
 
 import { IETF_LOCALE } from "@sudoo/locale";
 import { MDXProps } from "mdx/types";
+import { DEFAULT_LOCALE } from "../../i18n/config";
 import { useLocale } from "../../i18n/use-locale";
 import { markdownComponentsMap } from "../markdown/component-map";
 
 export type MDXLocaleSwitcherProps = {
 
     readonly parameters?: Record<string, any>;
+
+    readonly defaultLocale?: IETF_LOCALE;
 } & Partial<Record<IETF_LOCALE, React.FC<MDXProps>>>;
 
 export const MDXLocaleSwitcher: React.FC<MDXLocaleSwitcherProps> = (props: MDXLocaleSwitcherProps) => {
 
     const locale = useLocale();
-    const Component: React.FC<MDXProps> | undefined = props[locale];
+    const Component: React.FC<MDXProps> | undefined =
+        props[locale];
 
     if (!Component) {
+
+        const defaultLocale: IETF_LOCALE =
+            props.defaultLocale ?? DEFAULT_LOCALE;
+
+        const DefaultComponent: React.FC<MDXProps> | undefined =
+            props[defaultLocale];
+
+        if (DefaultComponent) {
+            return (<DefaultComponent
+                components={markdownComponentsMap}
+                parameters={props.parameters}
+            />);
+        }
+
         return (<div>
             {`Locale ${locale} not found`}
         </div>);
