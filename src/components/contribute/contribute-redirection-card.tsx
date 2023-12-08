fb480eaa700cc51aa68a576900be57e24e698788
wmxpy <wm@mengw.io>
2023-12-07 21:15:27 -0600

    Add ContributeRedirectionCard component

---
+++ src/components/contribute/contribute-redirection-card.tsx
@@ -0,0 +1,32 @@
+/**
+* @author WMXPY
+* @namespace Components_Contribute
+* @description Contribute Redirection Card
+*/
+
+import { IETF_LOCALE } from "@sudoo/locale";
+import * as React from "react";
+import { contributeInternationalization } from "../../dictionary/contribute/_intl";
+import { CONTRIBUTE_PROFILE } from "../../dictionary/contribute/_profile";
+import { HrefConfig } from "../../util/href";
+import { SIZE } from "../../util/size";
+import { RedirectionCard } from "../common/redirection-card";
+
+export type ContributeRedirectionCardProps = {
+
+    readonly locale: IETF_LOCALE;
+
+    readonly size?: SIZE;
+};
+
+export const ContributeRedirectionCard: React.FC<ContributeRedirectionCardProps> = (props: ContributeRedirectionCardProps) => {
+
+    const contributeFormat = contributeInternationalization.format(props.locale);
+
+    return (<RedirectionCard
+        title={contributeFormat.get(CONTRIBUTE_PROFILE.UNDERSTAND_HOW_TO_CONTRIBUTE)}
+        titleHref={HrefConfig.withinSite(props.locale, "contribute")}
+        subtitle={contributeFormat.get(CONTRIBUTE_PROFILE.UNDERSTAND_HOW_TO_CONTRIBUTE_DESCRIPTION)}
+        size={props.size}
+    />);
+};
