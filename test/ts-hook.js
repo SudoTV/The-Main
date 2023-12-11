15a88fcbb105163b88ecf041d973a52070c1b6b8
wmxpy <wm@mengw.io>
2023-12-10 20:57:49 -0600

    Update test configuration and dependencies

--- test/ts-hook.js
+++ test/ts-hook.js
@@ -19,33 +19,4 @@ ts_node.register({
     project: path.join(__dirname, "tsconfig.test.json"),
 });
 
-const baseURL = path.join(__dirname, "..", "src");
-
-console.log(baseURL);
-
-tsconfig_paths.register({
-    baseUrl: path.join(__dirname, "..", "src"),
-    paths: {
-        "@/components/*": [
-            "components/*",
-        ],
-        "@/data/*": [
-            "data/*",
-        ],
-        "@/dictionary/*": [
-            "dictionary/*",
-        ],
-        "@/error/*": [
-            "error/*",
-        ],
-        "@/i18n/*": [
-            "i18n/*",
-        ],
-        "@/styles/*": [
-            "styles/*",
-        ],
-        "@/util/*": [
-            "util/*",
-        ],
-    },
-});
+tsconfig_paths.register();
