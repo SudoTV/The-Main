dc9e30c9dc506259da5e4af140700adf119776a0
wmxpy <wm@mengw.io>
2023-12-18 00:33:10 -0600

    Update PowerLink component to support reverse arrow and replace option

--- src/components/common/power-link.tsx
+++ src/components/common/power-link.tsx
@@ -4,17 +4,19 @@
 * @description Power Link
 */
 
+import { HrefConfig } from "@/util/href";
 import Link from "next/link";
 import * as React from "react";
 import { BiLinkExternal } from "react-icons/bi";
-import { IoArrowForwardOutline } from "react-icons/io5";
-import { HrefConfig } from "@/util/href";
+import { IoArrowBackOutline, IoArrowForwardOutline } from "react-icons/io5";
 
 export type PowerLinkProps = {
 
     readonly href: HrefConfig;
     readonly children?: React.ReactNode;
 
+    readonly reverseArrow?: boolean;
+    readonly replace?: boolean;
     readonly noIcon?: boolean;
 };
 
@@ -26,7 +28,13 @@ export const PowerLink: React.FC<PowerLinkProps> = (props: PowerLinkProps) => {
             rel={props.href.external ? "noopener noreferrer" : undefined}
             target={props.href.external ? "_blank" : undefined}
             className="inline-flex hover:underline items-center gap-2 text-blue-500 dark:text-blue-300"
+            replace={props.replace}
         >
+            {props.noIcon
+                ? null
+                : props.reverseArrow && !props.href.external
+                    ? <IoArrowBackOutline />
+                    : null}
             <span>
                 {props.children}
             </span>
@@ -34,7 +42,9 @@ export const PowerLink: React.FC<PowerLinkProps> = (props: PowerLinkProps) => {
                 ? null
                 : props.href.external
                     ? <BiLinkExternal />
-                    : <IoArrowForwardOutline />}
+                    : props.reverseArrow
+                        ? null
+                        : <IoArrowForwardOutline />}
         </Link>
     );
 };
