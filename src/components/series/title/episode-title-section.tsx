78f5050788b8b4e31bf6c5fb8a853f79e9f5a966
wmxpy <wm@mengw.io>
2023-12-18 00:33:13 -0600

    Refactor episode title sections and add PowerLink

--- src/components/series/title/episode-title-section.tsx
+++ src/components/series/title/episode-title-section.tsx
@@ -4,10 +4,12 @@
 * @description Episode Title Section
 */
 
-import { IETF_LOCALE } from "@sudoo/locale";
-import * as React from "react";
+import { PowerLink } from "@/components/common/power-link";
 import { EPISODE_TYPE, EpisodeEntity } from "@/data/definition/episode/episode";
 import { SERIES_TYPE, SeriesEntity } from "@/data/definition/series/series";
+import { HrefConfig } from "@/util/href";
+import { IETF_LOCALE } from "@sudoo/locale";
+import * as React from "react";
 import { Description1 } from "../../typography/description-1";
 import { Header1 } from "../../typography/header-1";
 import { Section } from "../../typography/section";
@@ -29,7 +31,13 @@ export const EpisodeTitleSection: React.FC<EpisodeTitleSectionProps> = (
             <Description1
                 noMargin
             >
-                {props.series.title[props.locale]}
+                <PowerLink
+                    href={HrefConfig.withinSite(props.locale, "series", props.series.identifier)}
+                    reverseArrow
+                    replace
+                >
+                    {props.series.title[props.locale]}
+                </PowerLink>
             </Description1>
             <Header1>
                 {props.episode.title[props.locale]}
--- src/components/series/title/sub-episode-title-section.tsx
+++ src/components/series/title/sub-episode-title-section.tsx
@@ -4,15 +4,19 @@
 * @description Sub Episode Title Section
 */
 
+import { PowerLink } from "@/components/common/power-link";
+import { EPISODE_TYPE, EpisodeEntity } from "@/data/definition/episode/episode";
+import { SERIES_TYPE, SeriesEntity } from "@/data/definition/series/series";
+import { HrefConfig } from "@/util/href";
 import { IETF_LOCALE } from "@sudoo/locale";
 import * as React from "react";
-import { EPISODE_TYPE, EpisodeEntity } from "@/data/definition/episode/episode";
 import { Description1 } from "../../typography/description-1";
 import { Header1 } from "../../typography/header-1";
 import { Section } from "../../typography/section";
 
 export type SubEpisodeTitleSectionProps = {
 
+    readonly series: SeriesEntity<SERIES_TYPE>;
     readonly episode: EpisodeEntity<EPISODE_TYPE>;
 
     readonly title: React.ReactNode;
@@ -29,7 +33,13 @@ export const SubEpisodeTitleSection: React.FC<SubEpisodeTitleSectionProps> = (
             <Description1
                 noMargin
             >
-                {props.episode.title[props.locale]}
+                <PowerLink
+                    href={HrefConfig.withinSite(props.locale, "series", props.series.identifier, "episode", props.episode.identifier)}
+                    reverseArrow
+                    replace
+                >
+                    {props.episode.title[props.locale]}
+                </PowerLink>
             </Description1>
             <Header1>
                 {props.title}
